298c3c8593818baff98e2d9c13d766ef24c9f351
tonderaimuchada <37879739+tonderaimuchada@users.noreply.github.com>
2023-09-01 21:33:43 +0000

    Implemented getting card choices from the user

--- PokerHandEvaluator.java
+++ PokerHandEvaluator.java
@@ -2,16 +2,26 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Scanner;
 
 public class PokerHandEvaluator {
     private static Set<Card> cardsInHand;
     private static List<Card> deckOfCards;
 
     public static void main (String... args) {
-        System.out.println("Choose 5 unique cards for your hand");
+        System.out.println("Choose 5 unique cards for your hand. Press enter after every choice!");
         deckOfCards = loadDeckOfCards();
         for (int x=0; x<deckOfCards.size(); x++) {
             System.out.println("Select " + x + ". " + deckOfCards.get(x).getSuit() + " of " + deckOfCards.get(x).getKind());
+        }
+
+        int selectedIndexes[] = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        for (int y=0; y<selectedIndexes.length; y++) {
+            selectedIndexes[y] = Integer.parseInt(scanner.nextLine());
+        }
+        for (int y=0; y<selectedIndexes.length; y++) {
+            System.out.println(selectedIndexes[y]);
         }
         cardsInHand = new HashSet<>(deckOfCards);
     }
