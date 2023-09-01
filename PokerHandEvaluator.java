dd55652e77ed493b1bb468f8f84643e4949c7837
tonderaimuchada <37879739+tonderaimuchada@users.noreply.github.com>
2023-09-01 20:24:35 +0000

    Added Card class

--- PokerHandEvaluator.java
+++ PokerHandEvaluator.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class PokerHandEvaluator {
+    private List<Card> cardsList;
+    private List<Card> deckOfCards;
+
     public static void main (String... args) {
-        System.out.println("In main method");
+        System.out.println("Choose 5 cards for your hand");
+    }
+}
+
+class Card {
+    private String suit;
+    private String kind;
+
+    Card (String suit, String kind) {
+        this.suit = suit;
+        this.kind = kind;
+    }
+
+    public String getSuit(){
+        return suit;
+    }
+
+    public String getKind(){
+        return kind;
     }
 }
