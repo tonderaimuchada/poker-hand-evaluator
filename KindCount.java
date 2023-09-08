f91465e69c2e587c25c50d1e96dc435b596fe30c
tonderaimuchada <37879739+tonderaimuchada@users.noreply.github.com>
2023-09-08 16:25:22 +0000

    Pending changes exported from your codespace

---
+++ KindCount.java
@@ -0,0 +1,17 @@
+class KindCount {
+    int distinct;
+    int duplicate;
+
+    KindCount(int distinct, int duplicate) {
+        this.distinct = distinct;
+        this.duplicate = duplicate;
+    }
+    
+    int getDistinct() {
+        return this.distinct;
+    }
+
+    int getKind() {
+        return this.kind;
+    }
+}
